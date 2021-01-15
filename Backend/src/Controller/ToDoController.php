f1e9576b1a23d4ca7be4e3bcaac37f744b4cbf87
Tiago Pereira <tiagoopereira98@outlook.com>
2021-01-15 00:05:26 -0300

    Criação das APIs de CRUD dos ToDo

---
+++ Backend/src/Controller/ToDoController.php
@@ -0,0 +1,85 @@
+<?php 
+namespace App\Controller;
+
+use App\Repository\ToDoRepository;
+use App\Service\ToDoService;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class ToDoController extends AbstractController
+{
+    /** @var ToDoService */
+    private $toDoService;
+
+    /** @var ToDoRepository */
+    private $toDoRepository;
+
+    /** @var EntityManagerInterface */
+    private $entityManager;
+
+    public function __construct(
+        ToDoService $toDoService,
+        ToDoRepository $toDoRepository,
+        EntityManagerInterface $entityManager
+    )
+    {
+        $this->toDoService = $toDoService;
+        $this->toDoRepository = $toDoRepository;
+        $this->entityManager = $entityManager;
+    }
+
+    public function create(Request $request): Response
+    {
+        $json = $request->getContent();
+        $toDo = $this->toDoService->createEntity($json);
+
+        $this->entityManager->persist($toDo);
+        $this->entityManager->flush();
+
+        return new JsonResponse($toDo, Response::HTTP_CREATED);
+    }
+
+    public function getAll(): Response
+    {
+        $toDoList = $this->toDoRepository->findAll();
+
+        return new JsonResponse($toDoList, Response::HTTP_OK);
+    }
+
+    public function getOne(int $id): Response
+    {
+        $toDo = $this->toDoRepository->find($id);
+
+        return new JsonResponse($toDo, Response::HTTP_OK);
+    }
+
+    public function update(int $id, Request $request): Response
+    {
+        $json = $request->getContent();
+
+        /** @var ToDo $newToDo */
+        $newToDo = $this->toDoService->createEntity($json);
+        $existingToDo = $this->toDoRepository->find($id);
+
+        $existingToDo->setTitle($newToDo->getTitle());
+        $existingToDo->setDescription($newToDo->getDescription());
+        $existingToDo->setFinished($newToDo->getFinished());
+
+        $this->entityManager->flush();
+
+        return new JsonResponse($existingToDo, Response::HTTP_OK);
+    }
+
+    public function delete(int $id): Response
+    {
+        $toDo = $this->toDoRepository->find($id);
+
+        $this->entityManager->remove($toDo);
+        $this->entityManager->flush();
+
+        return new Response('', Response::HTTP_NO_CONTENT);
+    }
+}
