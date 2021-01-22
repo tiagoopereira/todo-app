6e7418392c168d33b266346500f3b82c01ce9f9a
Tiago Pereira <tiagoopereira98@outlook.com>
2021-01-21 22:18:37 -0300

    Resolvendo problema de CORS (temporário)

--- Backend/public/index.php
+++ Backend/public/index.php
@@ -7,6 +7,18 @@ use Symfony\Component\HttpFoundation\Request;
 
 require dirname(__DIR__).'/vendor/autoload.php';
 
+ // CORS HEADERS
+ header('Access-Control-Allow-Origin: *');
+ header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+ header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+ header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+ 
+ $method = $_SERVER['REQUEST_METHOD'];
+
+ if ($method == "OPTIONS") {
+     die();
+ }
+
 (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
 
 if ($_SERVER['APP_DEBUG']) {
