46b66310c8abef1b21c8119780f5f09b7b61b5c9
Tiago Pereira <tiagoopereira98@outlook.com>
2021-01-21 22:55:20 -0300

    Início do frontend da aplicação (Sofrência)

---
+++ Frontend/js/Controller/toDoCtrl.js
@@ -0,0 +1,31 @@
+angular.module("toDoApp").controller("toDoCtrl", function($scope, $http) {
+    var loadToDoList = function() {
+        $http({
+            method: 'GET',
+            url: 'http://localhost/todo'
+        }).then(function(response) {
+            $scope.toDoList = response.data;
+        });
+    };
+
+    $scope.submit = function() {
+        $http({
+            method: 'POST',
+            url: 'http://localhost/todo',
+            data: { description: $scope.description }
+        }).then(function(response) {
+            $scope.toDoList.push(response.data);
+        });
+    };
+
+    $scope.deleteToDo = function(id) {
+        $http({
+            method: 'DELETE',
+            url: `http://localhost/todo/${id}`
+        }).then(function(response) {
+            loadToDoList();
+        });
+    };
+
+    loadToDoList();
+});
